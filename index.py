fdc61d3fd2fd7b6332a056e0dc0d1d298dc46421
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-04-24 23:33:02 -0400

    Just code

--- index.py
+++ index.py
@@ -3,7 +3,4 @@ mevalar = ["olma", "banan", "nok", "anor"]
 # Ro'yxatni chiqarish
 print("Birinchi meva:", mevalar[0])
 
-# Ro'yxatni barcha elementlarini chiqarish
-print("Barcha mevalar:")
-for meva in mevalar:
-    print(meva)
+ 
