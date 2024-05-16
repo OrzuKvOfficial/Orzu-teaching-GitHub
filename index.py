576a7ed9e94a05cf511e14971711d5ec757e349f
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-05-15 22:43:47 -0400

    Delivery at Js

--- index.py
+++ index.py
@@ -21,3 +21,47 @@ while True:
 # Qisqa bo'lish
 cap.release()
 cv2.destroyAllWindows()
+# Toplama işlemi
+def toplama(x, y):
+    return x + y
+
+# Çıkarma işlemi
+def cikarma(x, y):
+    return x - y
+
+# Çarpma işlemi
+def carpma(x, y):
+    return x * y
+
+# Bölme işlemi
+def bolme(x, y):
+    if y == 0:
+        return "Bir sayıyı sıfıra bölemezsiniz!"
+    else:
+        return x / y
+
+print("Yapmak istediğiniz işlemi seçin:")
+print("1. Toplama")
+print("2. Çıkarma")
+print("3. Çarpma")
+print("4. Bölme")
+
+secim = input("Seçiminizi yapın (1/2/3/4): ")
+
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+if secim == '1':
+    print(sayi1, "+", sayi2, "=", toplama(sayi1, sayi2))
+
+elif secim == '2':
+    print(sayi1, "-", sayi2, "=", cikarma(sayi1, sayi2))
+
+elif secim == '3':
+    print(sayi1, "*", sayi2, "=", carpma(sayi1, sayi2))
+
+elif secim == '4':
+    print(sayi1, "/", sayi2, "=", bolme(sayi1, sayi2))
+
+else:
+    print("Geçersiz giriş")
