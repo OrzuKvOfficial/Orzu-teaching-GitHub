8ee51dcfe84197ac732572c0e0353acafcf76713
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-05-19 21:57:05 -0400

    Connecting to a phone via bluetooth in Py

--- index.py
+++ index.py
@@ -142,3 +142,23 @@ engine = create_engine('sqlite:///output.db')
 df.to_sql('table_name', con=engine, if_exists='replace', index=False)
 
 print("Ma'lumotlar muvaffaqiyatli SQL bazaga o'tkazildi va 'output.db' fayliga saqlandi.")
+import bluetooth
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+port = 1
+server_sock.bind(("", port))
+server_sock.listen(1)
+
+print("Kuting, ulanishlar kutilmoqda...")
+client_sock, address = server_sock.accept()
+print(f"Ulangan manzil: {address}")
+
+while True:
+    data = client_sock.recv(1024)
+    if not data:
+        break
+    print(f"Olingan ma'lumot: {data.decode('utf-8')}")
+    client_sock.send(data)
+
+client_sock.close()
+server_sock.close()
